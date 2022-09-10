49563890c05cd55bf06704249589b2b9e368ad7e
annie-82 <92351283+annie-82@users.noreply.github.com>
2022-09-10 13:24:04 -0500

    Update CH9PB4.py

--- CH9PB4.py
+++ CH9PB4.py
@@ -3,6 +3,14 @@
 # Problem 4 :- Creating a while loop that initializes a counter at 0 and will run until the counter reaches 50.
 # If the value of the counter is divisible by 10, append the value to the list called tens.
 # Confirm the list results using a print statement.
+
+#The changed question: 
+# This program uses a while loop
+# It creates a list called L that contains the numbers 10 to 20.
+# On each iteration, the loop appends the current value of a counter variable to the list "L".
+# and then increase the counter by 1.
+# The while loop stops once the counter variable is greater than 20 and prints the final list "L".
+
 counter = 0
 tens=[]
 while(counter<=50):
